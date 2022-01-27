207421fe8f1171ce087cca9b9dd3b056d3305028
jim-nnamdi <jimsamuel50@gmail.com>
2022-01-27 12:38:07 +0100

    clean ups and referencing

--- src/bin/publish_coin.rs
+++ src/bin/publish_coin.rs
@@ -1,20 +1,24 @@
 extern crate diesel;
 extern crate coins;
 
-use self::models::post;
+use self::diesel::prelude::*;
 use self::coins::*;
-use diesel::prelude::*;
-use std::env::args
+use std::env::args;
 
 fn main(){
 
-  use diesel::schema::coins::dsl::{coins, approved};
+  use self::schema::coins::dsl::{coins, approved};
 
-  let id = args().nth(1).expect("approval needs an id").parse::<i32>.expect("invalid coin id provided");
+  let id = args().nth(1).expect("approval needs an id").parse::<i32>().expect("invalid coin id provided");
 
   let connection = establish_connection();
 
-  let coin = diesel.update(coins.find(id)).set(approved.eq(true)).execute(conn).expect(&format!("unable to find {}", id));
+  let _ = diesel::update(coins.find(id)).set(approved.eq(true)).execute(&connection).expect(&format!("unable to find {}", id));
 
-  println!("Coin data updated .... name: {}", coin.coinname);
+  let _coin: models::Coin = coins
+  .find(id)
+  .first(&connection)
+  .unwrap_or_else(|_| panic!("Unable to find coin {}", id));
+
+  println!("... {} coin data updated with rust !", _coin.coinname);
 }
